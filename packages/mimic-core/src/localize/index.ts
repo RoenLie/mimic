7b9e90791c23d959abff4da4bd01e038e3525d13
KristofferRoenLie <KristofferRoenLie@gmail.com>
2023-04-17 00:00:05 +0200

    attempt 5

---
+++ packages/mimic-core/src/localize/index.ts
@@ -0,0 +1,7 @@
+/* eslint-disable simple-import-sort/exports */
+/* eslint-disable max-len */
+/* auto generated */
+export type { DynamicImport, LanguageExport, FunctionParams, LocalizeDirection, LanguageCode, Translation, DefaultTranslation } from './localize.js';
+export { registerTranslation, update, localizeData } from './localize.js';
+export { Localize } from './localize-service.js';
+export { translationLoader } from './loader.js';
