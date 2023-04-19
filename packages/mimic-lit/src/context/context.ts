3a795daa760ae32764170eae15914c57eb07976c
KristofferRoenLie <kristoffer.roenlie@eye-share.no>
2023-04-19 11:41:10 +0200

    Updated context with changes done in another project.

--- packages/mimic-lit/src/context/context.ts
+++ packages/mimic-lit/src/context/context.ts
@@ -1,22 +1,26 @@
-import { RecordOf } from '@roenlie/mimic-core/types';
 import { LitElement } from 'lit';
+import { state } from 'lit/decorators.js';
 
 
+type RecordOf<T> = T & Record<keyof any, any>;
 type ConsumeContextEvent = CustomEvent<{value: any}>;
 
 
 const createEventName = (prop: string) => 'consume-context:' + prop;
+const createHydrateName = (prop: string) => 'hydrate-context:' + prop;
 
 
-export const provide = () => {
+export const provide = (name: string) => {
 	return (target: RecordOf<LitElement>, prop: string) => {
 		const connected = target.connectedCallback;
 		const disconnected = target.disconnectedCallback;
+		const update = target['update'];
 
-		const eventName = createEventName(prop);
+		const hydrateName = createHydrateName(name);
+		const eventName = createEventName(name);
 		let provideHandler: (ev: Event) => any;
 
-		target['connectedCallback'] = function() {
+		target.connectedCallback = function() {
 			provideHandler = (ev: Event) => {
 				ev.preventDefault();
 				ev.stopPropagation();
@@ -32,35 +36,71 @@ export const provide = () => {
 			connected.call(this);
 		};
 
-		target['disconnectedCallback'] = function() {
+		target.disconnectedCallback = function() {
 			this.removeEventListener(eventName, provideHandler);
 
 			disconnected.call(this);
 		};
+
+		target['update'] = function(changedProperties) {
+			update.call(this, changedProperties);
+
+			if (changedProperties.has(prop)) {
+				const ev = new CustomEvent(hydrateName, {
+					bubbles:  true,
+					composed: true,
+				});
+
+				this.dispatchEvent(ev);
+			}
+		};
+
+		return state()(target, prop);
 	};
 };
 
 
-export const consume = () => {
+export const consume = (name: string) => {
 	return (target: RecordOf<LitElement>, prop: string) => {
 		const connected = target.connectedCallback;
-		const eventName = createEventName(prop);
+		const disconnected = target.disconnectedCallback;
 
-		target['connectedCallback'] = function() {
-			const event = new CustomEvent(eventName, {
-				bubbles:  true,
-				composed: true,
-				detail:   { value: undefined },
-			});
-			this.dispatchEvent(event);
+		const eventName = createEventName(name);
+		const hydrateName = createHydrateName(name);
 
-			const value = event.detail.value;
-			if (value)
-				this[prop] = value;
-			else
-				console.error('Could not consume ' + prop);
+		let hydrateHandler: (ev: Event) => any;
+
+		target.connectedCallback = function() {
+			const request = () => {
+				const event = new CustomEvent(eventName, {
+					bubbles:  true,
+					composed: true,
+					detail:   { value: undefined },
+				});
+				this.dispatchEvent(event);
+
+				const value = event.detail.value;
+				if (value !== undefined)
+					this[prop] = value;
+				else
+					console.error('Could not consume ' + name);
+			};
+
+			request();
+
+			hydrateHandler = () => request();
+			globalThis.removeEventListener(hydrateName, hydrateHandler);
+			globalThis.addEventListener(hydrateName, hydrateHandler);
 
 			connected.call(this);
 		};
+
+		target.disconnectedCallback = function() {
+			globalThis.removeEventListener(hydrateName, hydrateHandler);
+
+			disconnected.call(this);
+		};
+
+		return state()(target, prop);
 	};
 };
