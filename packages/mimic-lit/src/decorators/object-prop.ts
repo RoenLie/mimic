a9c986ce98a2ba626908c4b1eeb18befccb07bfc
KristofferRoenLie <KristofferRoenLie@gmail.com>
2023-07-03 22:12:42 +0200

    Fixed typing issue in decorator.

--- packages/mimic-lit/src/decorators/object-prop.ts
+++ packages/mimic-lit/src/decorators/object-prop.ts
@@ -1,6 +1,7 @@
 import { deepEqual } from 'fast-equals';
+import { PropertyDeclaration } from 'lit';
 import { property } from 'lit/decorators.js';
 
 
-export const objectProp = (...options: Parameters<typeof property>): ((protoOrDescriptor: object, name?: PropertyKey | undefined) => any) =>
+export const objectProp = (options: PropertyDeclaration): ((protoOrDescriptor: object, name?: PropertyKey | undefined) => any) =>
 	property({ type: Object, attribute: false, hasChanged: (v, o) => deepEqual(v, o), ...options });
