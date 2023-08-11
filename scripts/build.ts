8d184439f2826b6e085198d595c709373ac0683a
KristofferRoenLie <KristofferRoenLie@gmail.com>
2023-08-12 00:50:29 +0200

    .

--- scripts/build.ts
+++ scripts/build.ts
@@ -4,8 +4,6 @@ import { packageBuildOrder } from './utilities/find-build-order.js';
 
 const buildOrder = await packageBuildOrder();
 
-console.log(buildOrder);
-
 
 for await (const cmds of buildOrder)
 	await Promise.all(cmds.map(cmd => execPromise(`pnpm --filter=${ cmd } run build`)));
