ef6c144be84fe519fef421d89e0ceedd55de8045
KristofferRoenLie <KristofferRoenLie@gmail.com>
2023-08-25 00:09:13 +0200

    Updated state store and deps.

---
+++ packages/state-store/demo/main.ts
@@ -0,0 +1,102 @@
+import { range } from '@roenlie/mimic-core/array';
+import { css, html, LitElement } from 'lit';
+import { customElement, state } from 'lit/decorators.js';
+import { map } from 'lit/directives/map.js';
+import { styleMap } from 'lit/directives/style-map.js';
+
+import { StateStore } from '../src/store.js';
+
+
+class TestStore extends StateStore {
+
+	public counter = { value: 0 };
+
+}
+
+const testStore = new TestStore();
+
+
+@customElement('demo-main')
+export class MainCmp extends LitElement {
+
+	@state() protected counters: HTMLElement[] = range(0, 500)
+		.map(() => document.createElement('demo-counter'));
+
+	protected counterInterval?: ReturnType<typeof setInterval>;
+	protected templateInterval?: ReturnType<typeof setInterval>;
+
+	public override connectedCallback(): void {
+		super.connectedCallback();
+
+		this.counterInterval = setInterval(() => {
+			testStore.update('counter', (value) => {
+				value.value++;
+			});
+		}, 10);
+
+		this.templateInterval = setInterval(() => {
+			this.counters = range(0, 500)
+				.map(() => document.createElement('demo-counter'));
+
+			testStore.counter.value = 0;
+		}, 5000);
+	}
+
+	protected override render() {
+		return html`
+		${ map(this.counters, counter => counter) }
+		`;
+	}
+
+	public static override styles = [
+		css`
+		:host {
+			display: grid;
+			grid-template-columns: repeat(25, 30px);
+			grid-auto-rows: 30px;
+		}
+		`,
+	];
+
+}
+
+
+@customElement('demo-counter')
+export class CounterCmp extends LitElement {
+
+	public override connectedCallback(): void {
+		super.connectedCallback();
+		testStore.connect(this, 'counter');
+	}
+
+
+	protected override render() {
+		const fontSize = Math.ceil(Math.random() * 16) + 'px';
+		const r = Math.ceil(Math.random() * 255);
+		const g = Math.ceil(Math.random() * 255);
+		const b = Math.ceil(Math.random() * 255);
+		const bg = `rgb(${ r } ${ g } ${ b })`;
+
+		return html`
+		<div style=${ styleMap({
+			'background-color': bg,
+			'font-size':        fontSize,
+		}) }>
+			${ testStore.counter.value }
+		</div>
+		`;
+	}
+
+	public static override styles = [
+		css`
+		:host {
+			display: grid;
+		}
+		div {
+			display: grid;
+			place-items: center;
+		}
+		`,
+	];
+
+}
---
+++ packages/state-store/src/store.ts
@@ -0,0 +1,215 @@
+import { lazyMap, lazyWeakmap } from '@roenlie/mimic-core/structs';
+import type { ReactiveControllerHost } from 'lit';
+
+
+export interface UpdatableElement {
+	requestUpdate: Function;
+}
+
+
+export class StateStore {
+
+	/** Handlers used through the proxy, which intercept any get or set on the store. */
+	static #proxyHandlers = {
+		get(target: StateStore, key: keyof StateStore) {
+			if (key as string === '__origin')
+				return target;
+
+			return target[key];
+		},
+		set(target: StateStore, key: keyof StateStore, value: any) {
+			if (key in target && target[key] === value)
+				return true;
+
+			try {
+				return Reflect.set(target, key, value);
+			}
+			finally {
+				StateStore.#notify(target, key);
+			}
+		},
+	};
+
+	/** Runs rerender requests and the listener functions upon change or update of a property. */
+	static #notify(target: StateStore, key: keyof StateStore) {
+		const observerMap = StateStore.#observers.get(target);
+		for (const [ _key, set ] of observerMap ?? []) {
+			if (_key !== key)
+				continue;
+
+			for (const ref of set) {
+				const el = ref.deref();
+				!el ? set.delete(ref) : el.requestUpdate();
+			}
+		}
+
+		const listenerMap = StateStore.#listeners.get(target);
+		for (const map of listenerMap?.values() ?? []) {
+			const listeners = map.get(key);
+			listeners?.forEach(listener => listener());
+		}
+	}
+
+	static #observers = new WeakMap<StateStore, Map<string, Set<WeakRef<UpdatableElement>>>>();
+	static #listeners = new WeakMap<StateStore, Map<object, Map<string, Set<Function>>>>();
+	static #refRegistry = new FinalizationRegistry<{origin: StateStore; ref: WeakRef<any>;}>(
+		({ origin, ref }) => {
+			const map = StateStore.#observers.get(origin);
+			for (const set of map?.values() ?? [])
+				set.delete(ref);
+		},
+	);
+
+	/**
+	 * This is accessed through the proxy,
+	 * as that's the only scenario you need to get a reference to the origin.
+	 */
+	private __origin: this;
+
+	constructor() {
+		return new Proxy<StateStore>(this, StateStore.#proxyHandlers);
+	}
+
+	/**
+	 * Updates a value in the store, without replacing the original reference.
+	 * Causes listeners and observers to activate after use.
+	 */
+	public update<T extends keyof Omit<this, keyof StateStore>>(
+		prop: T,
+		/**
+		 * Function that mutates the object in the store.
+		 * Return false to not trigger a rerender.
+		 */
+		mutate: (value: this[T]) => void | false,
+	) {
+		const rerender = mutate(this.__origin[prop]);
+		if (rerender ?? true)
+			StateStore.#notify(this.__origin, prop as keyof StateStore);
+	}
+
+	/**
+	 * Registers a listener function that runs when the property is either set or updated.
+	 */
+	public listen(
+		object: object,
+		prop: keyof Omit<this, keyof StateStore>,
+		func: () => any,
+	) {
+		const _prop = prop as string;
+
+		const elementMap = lazyWeakmap(StateStore.#listeners, this.__origin, () => new Map());
+		const propMap = lazyMap(elementMap, object, () => new Map());
+		const funcSet = lazyMap(propMap, _prop, () => new Set());
+
+		funcSet.add(func);
+	}
+
+	/** Removes a listener from the store. */
+	public unlisten(
+		object: object,
+		prop: keyof Omit<this, keyof StateStore>,
+		func: () => void,
+	) {
+		const _prop = prop as string;
+
+		const elementMap = lazyWeakmap(StateStore.#listeners, this.__origin, () => new Map());
+		const propMap = lazyMap(elementMap, object, () => new Map());
+		const funcSet = lazyMap(propMap, _prop, () => new Set());
+
+		funcSet.delete(func);
+	}
+
+	/** Removes all listeners in the store from its connected object */
+	public unlistenAll(element: object) {
+		StateStore.#listeners.get(this.__origin)?.get(element)?.forEach(set => set.clear());
+		StateStore.#listeners.get(this.__origin)?.get(element)?.clear();
+	}
+
+	/**
+	 * Registers the supplied element as wanting to be rerendered
+	 * upon a set or update of the properties supplied
+	 */
+	public observe(
+		element: UpdatableElement,
+		...props: (keyof Omit<this, keyof StateStore>)[]
+	) {
+		const map = lazyWeakmap(StateStore.#observers, this.__origin, () => new Map());
+
+		for (const prop of props as string[]) {
+			if (!(prop in this.__origin))
+				throw new Error('property: ' + prop + ' does not exist in the store.');
+
+			const propMap = lazyMap(map, prop, new Set());
+			const ref = new WeakRef(element);
+			propMap.add(ref);
+
+			StateStore.#refRegistry.register(element, { origin: this.__origin, ref }, element);
+		}
+	}
+
+	/** Removes a property from the supplied elements observed list. */
+	public unobserve(
+		element: UpdatableElement,
+		...props: (keyof Omit<this, keyof StateStore>)[]
+	) {
+		const map = StateStore.#observers.get(this.__origin);
+		for (const prop of props as string[]) {
+			const set = map?.get(prop);
+			for (const ref of set ?? []) {
+				if (ref.deref() !== element)
+					continue;
+
+				set?.delete(ref);
+				StateStore.#refRegistry.unregister(element);
+			}
+		}
+	}
+
+	/** Removes all observed properties from an element reference. */
+	public unobserveAll(element: UpdatableElement) {
+		const map = StateStore.#observers.get(this.__origin);
+		for (const set of map?.values() ?? []) {
+			for (const ref of set) {
+				if (ref.deref() !== element)
+					continue;
+
+				set.delete(ref);
+				StateStore.#refRegistry.unregister(element);
+			}
+		}
+	}
+
+	/**
+	 * Connects a controller to the supplied element.
+	 * Allowing the controller to automatically handle cleanup
+	 * of any observers or listeners upon disconnecting.
+	 * Also takes in a rest arg of props, which are props that will be observed
+	 */
+	public connect(
+		element: ReactiveControllerHost,
+		...props: (keyof Omit<this, keyof StateStore>)[]
+	) {
+		this.observe(element, ...props);
+
+		element.addController({
+			hostDisconnected: () => {
+				this.unlistenAll(element);
+				this.unobserveAll(element);
+			},
+		});
+	}
+
+	/** Completely removes all listeners and observers from the store this is used from. */
+	public dispose() {
+		StateStore.#observers.get(this.__origin)?.forEach(map => {
+			map.clear();
+		});
+		StateStore.#observers.delete(this.__origin);
+
+		StateStore.#listeners.get(this.__origin)?.forEach(map => {
+			map.forEach(set => set.clear()), map.clear();
+		});
+		StateStore.#listeners.delete(this.__origin);
+	}
+
+}
