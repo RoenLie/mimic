07c6f5d01157859571b8c260b3db944fe5e81b07
KristofferRoenLie <KristofferRoenLie@gmail.com>
2024-01-23 23:15:36 +0100

    custom injectable function that no longer throws if injectable has already been used on a class. Why should it throw? it can just return as there is no harm in checking..

--- packages/lit-aegis/src/annotations/annotations.ts
+++ packages/lit-aegis/src/annotations/annotations.ts
@@ -1,7 +1,7 @@
 import { useReflectMetadata } from '@roenlie/reflect-metadata';
 import {
 	inject as _inject,
-	injectable as _injectable,
+	METADATA_KEY,
 	multiInject as _multiInject,
 	named as _named,
 	tagged as _tagged,
@@ -11,8 +11,20 @@ import {
 useReflectMetadata();
 
 
+export const injectable = () => {
+	return <T extends abstract new(...args: any) => unknown>(target: T) => {
+		if (Reflect.hasOwnMetadata(METADATA_KEY.PARAM_TYPES, target))
+			return target;
+
+		const types = Reflect.getMetadata(METADATA_KEY.DESIGN_PARAM_TYPES, target) || [];
+		Reflect.defineMetadata(METADATA_KEY.PARAM_TYPES, types, target);
+
+		return target;
+	};
+};
+
+
 export const inject = _inject;
-export const injectable = _injectable;
 export const multiInject = _multiInject;
 export const named = _named;
 export const tagged = _tagged;
